bd48e1e47e735699b50079aad1c0299efb96c993
rustynerve <hsingchien.zhang@gmail.com>
2022-11-01 00:37:20 -0700

    layouts and resizing etc

--- gui/app.py
+++ gui/app.py
@@ -1,20 +1,19 @@
-from qtpy import QtCore, QtGui
-from qtpy.QtCore import Qt, QEvent
+from ast import arg
+from MainWindow import Ui_MainWindow
+from typing import Optional, Union
+import sys
+from PyQt5 import QtWidgets, uic
 
-from qtpy.QtWidgets import QApplication, QMainWindow, QWidget, QDockWidget
-from qtpy.QtWidgets import QVBoxLayout, QHBoxLayout, QGroupBox
-from qtpy.QtWidgets import QLabel, QPushButton, QComboBox
-from qtpy.QtWidgets import QMessageBox
 
-from typing import Callable, List, Optional, Tuple
-
-class MainWindow(QMainWindow):
-    def __init__(self, parent: typing.Optional[QWidget] = ..., flags: typing.Union[QtCore.Qt.WindowFlags, QtCore.Qt.WindowType] = ...) -> None:
-        super().__init__(parent, flags):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        
-        self.setAcceptDrops(True)
-
-        self.neurons = Nuron()
+        self.setupUi(self)
 
 
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
+print("Happy screening!")
